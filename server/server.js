10ee887bd59e06ca0d19783368f221dd40be328d
unknown <txldarkrider@gmail.com>
2020-07-09 13:36:59 -0400

    Added connection and disconnection messages

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const publicPath =path.join(__dirname,"/../public");
 const port = process.env.PORT || 3000;
-
 let app = express();
+
+let server = http.createServer(app);
+let io = socketio(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on("connection",(socket)=>{
+    console.log("Connection Established");
+});
+io.on("disconnect",()=>{
+    console.log("User Was Disconnected");
+})
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 });
